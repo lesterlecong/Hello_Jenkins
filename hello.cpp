1ecd0bd5caccc6a8bc4eedd37f4cf8c3cbe174d5
Lester Lecong <lester_lecong@yahoo.com>
2019-02-01 15:26:21 +0800

    Add more prints.

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, Lester\n";
+  cout << "Hello, Everyone!!!\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
